201324f9999ab8e117c29c69c8f2664e990db3c7
Pablo Serrano <pablo@mobiground.com>
2012-07-26 12:31:24 +0200

    change comment to english

--- iotest.sh
+++ iotest.sh
@@ -1,24 +1,24 @@
 #/bin/bash 
-# Test de iops para sistemas y vps 
+# Test iops for servers and vps 
 # By Pablo Serrano 
 
-echo "Bajando ioping"
+echo "Download ioping"
 wget http://ioping.googlecode.com/files/ioping-0.6.tar.gz
-echo "Montando ioping"
+echo "Make ioping"
 tar xvzf ioping-0.6.tar.gz
 cd ioping-0.6
 make 
 make install
 cd ..
 
-echo "test de disco en directorio de ejecución"
+echo "test disk on main dir"
 
 ioping . -c 10
 
-echo "test exaustivo de /home/"
+echo "Exaustive test on /home/"
 
 ioping /home/ -R
 
-echo "Prueba de rendimiento con dd de generacion de un fichero de 1 giga."
+echo "Stress test with dd. With zero contain 1G file."
 
 dd if=/dev/zero of=test bs=64k count=16k conv=fdatasync
