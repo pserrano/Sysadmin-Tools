3e5768d05bc4f3428fedb2a680db3985660a286e
Pablo Serrano <pablo@mobiground.com>
2012-08-02 13:45:23 +0200

    add others servers

--- speedtest.sh
+++ speedtest.sh
@@ -1,7 +1,12 @@
 #/bin/bash
+#Speedtest.sh by Pablo Serrano. 
+# This script test you dw connection from others servers 
 
-echo "test form usa site"
+echo "Test from usa site"
 wget --output-document=/dev/null http://speedtest.wdc01.softlayer.com/downloads/test500.zip
 
 echo "test from CDN multisite"
-wget --output-document=/dev/null http://cachefly.cachefly.net/100mb.test
+wget --output-document=/dev/null http://cachefly.cachefly.net/100mb.test 
+
+echo "Test from EUR site"
+wget --output-document=/dev/null http://mirror.leaseweb.com/speedtest/100mb.bin
